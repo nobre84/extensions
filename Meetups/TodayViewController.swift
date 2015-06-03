3002256ba7a8921d3dd5caec5748c77f76c078e5
Rafael Nobre <nobre84@gmail.com>
2015-06-03 01:47:12 -0300

    Outlets, json etc

--- Meetups/TodayViewController.swift
+++ Meetups/TodayViewController.swift
@@ -9,13 +9,22 @@
 import UIKit
 import NotificationCenter
 import Alamofire
+import SwiftyJSON
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDataSource {
     
     let meetupsSuiteName = "group.br.com.Extensions.Meetup.Shared"
-    var apiKey: String?
+    var apiKey = ""
     let standardApiKey = ""
-        
+    var comments = [AnyObject]()
+    
+    // UI
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbCount: UILabel!
+    @IBOutlet weak var lbTimeLocation: UILabel!
+    @IBOutlet weak var tbComments: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
@@ -36,15 +45,34 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
         // Perform any setup necessary in order to update the view.
 
-//        Alamofire.request(.GET, "http://meetup.com", parameters: ["key":apiKey!])
-//                 .responseJSON(options: nil) { (, , , ) -> Void in
-//                    
-//        }
+        Alamofire.request(.GET, "http://meetup.com", parameters: ["key":apiKey]).responseJSON(options: nil) { (Request, Response, jsonData, error) -> Void in
+            let json = JSON(jsonData)
+            println(json)
+            completionHandler(NCUpdateResult.NewData)
+        }
+        
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
 
-        completionHandler(NCUpdateResult.NewData)
     }
     
+    // TableView Data Source
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return nil
+    }
+    
+    @IBAction func commentAction(sender: AnyObject) {
+    }
+    
+    @IBAction func seeAllAction(sender: AnyObject) {
+    }
+    
+    @IBAction func configureAction(sender: AnyObject) {
+    }
 }
